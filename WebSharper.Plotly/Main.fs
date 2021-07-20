2360bc56b1259cd890ac8de5a54f96b1c9cbfe78
steiner2001 <steiner.akos.2001@gmail.com>
2021-07-20 18:06:20 +0200

    changes in main

--- WebSharper.Plotly/Main.fs
+++ WebSharper.Plotly/Main.fs
@@ -142,46 +142,29 @@ module Definition =
 
     let Options = OptionsModule.Options
 
-    let PlotObject =
-        Class "PlotObject"
-        |+> Pattern.RequiredFields [
-            "data", Data
-            "layout", Layout.Type
-        ]
-
-    let Frames =
-        Class "Frames"
-        |+> Pattern.RequiredFields [
-            "name", T<string>
-            "data", Data
-        ]
-
     let Plotly =
         Class "Plotly"
         |+> Static [
             "newPlot" => WithTypes Types (fun t -> (T<string> + T<HTMLElement>) * !| t * !? Layout * !? Options ^-> T<HTMLElement>)
             "newPlot" => (T<string> + T<HTMLElement>) * !| CommonModule.Trace * !? Layout * !? Options ^-> T<HTMLElement>
-            "react" =>
-                WithTypes Types (fun t -> (T<string> + T<HTMLElement>) * !|t * !?Layout * !? Options ^-> T<HTMLElement>)
-            "restyle" => (T<string> + T<HTMLElement>) * T<obj> * !? (!| T<int>) ^-> T<HTMLElement> // TODO: update
-            "relayout" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement> // TODO: update
-            "update" => (T<string> + T<HTMLElement>) * T<obj> * T<obj> * !? (!| T<int>) ^-> T<HTMLElement> // TODO: both objects
-            "validate" =>
-                WithTypes Types (fun t -> !|t * Layout ^-> T<HTMLElement>)
-            "makeTemplate" =>
-                WithTypes Types (fun t -> PlotObject + T<HTMLElement> ^-> T<HTMLElement>)
-            "validateTemplate" =>
-                WithTypes Types (fun t -> (PlotObject + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>)
+            "react" => WithTypes Types (fun t -> (T<string> + T<HTMLElement>) * !|t * !?Layout * !? Options ^-> T<HTMLElement>)
+            "react" => (T<string> + T<HTMLElement>) * !| CommonModule.Trace * !?Layout * !? Options ^-> T<HTMLElement>
+            "restyle" => (T<string> + T<HTMLElement>) * T<obj> * !? (!| T<int>) ^-> T<HTMLElement>
+            "relayout" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
+            "update" => (T<string> + T<HTMLElement>) * T<obj> * T<obj> * !? (!| T<int>) ^-> T<HTMLElement>
+            "validate" => !| T<obj> * Layout ^-> T<HTMLElement>
+            "makeTemplate" => T<obj> + T<HTMLElement> ^-> T<HTMLElement>
+            "validateTemplate" => (T<obj> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
             "addTraces" => (T<string> + T<HTMLElement>) * Data ^-> T<HTMLElement>
             "deleteTraces" => (T<string> + T<HTMLElement>) * (T<int> + !| T<int>) ^-> T<HTMLElement>
             "moveTraces" => (T<string> + T<HTMLElement>) * (T<int> + !| T<int>) * !? (T<int> + !| T<int>) ^-> T<HTMLElement>
             "extendTraces" => (T<string> + T<HTMLElement>) * Data * !| T<int> * !? T<int> ^-> T<HTMLElement>
             "prependTraces" => (T<string> + T<HTMLElement>) * Data * !| T<int> * !? T<int> ^-> T<HTMLElement>
-            "addFrames" => (T<string> + T<HTMLElement>) * Frames ^-> T<HTMLElement>
-            "animate" => (T<string> + T<HTMLElement>) * Frames ^-> T<HTMLElement> // Super TODO
+            "addFrames" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
+            "animate" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
             "purge" => (T<string> + T<HTMLElement>) ^-> T<HTMLElement>
-            "toImage" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement> // TODO
-            "downloadImage" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement> // TODO
+            "toImage" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
+            "downloadImage" => (T<string> + T<HTMLElement>) * T<obj> ^-> T<HTMLElement>
         ]
 
     let Assembly =
@@ -244,8 +227,6 @@ module Definition =
                 OptionsModule.OptionsNamespaces
                 [
                     Common.CommonModule.Trace
-                    PlotObject
-                    Frames
                     Plotly
                 ]
             ])

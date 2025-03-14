6374402efa08450fd59c2188fc6ffd3f70ff213f
András Jankó <andras.janko@gmail.com>
2025-03-14 13:14:43 +0100

    updates for WebSharper 8

---
+++ WebSharper.Plotly.Tests/esbuild.config.mjs
@@ -0,0 +1,14 @@
+﻿import { build } from 'esbuild'
+
+var options =
+{
+  entryPoints: ['./build/WebSharper.Plotly.Tests.js'],
+  bundle: true,
+  minify: true,
+  format: 'iife',
+  outfile: '../build/Scripts/WebSharper.Plotly.Tests.min.js',
+  globalName: 'wsbundle'
+};
+
+build(options);
+

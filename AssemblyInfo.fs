8d8b45e5fee785901f9de494e57167d0d0b4bc03
intellifactory-gh <intellifactory-gh@users.noreply.github.com>
2025-04-16 15:29:35 +0000

    deploy: 949963b9f0588fa64fa244b01a27cb73d6db00b4

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -2,8 +2,8 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("8.0.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("8.0.0.1")>]
+[<assembly: AssemblyVersionAttribute("9.0.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("9.0.0.1")>]
 [<assembly: AssemblyCompanyAttribute("IntelliFactory")>]
 [<assembly: AssemblyCopyrightAttribute("(c) IntelliFactory 2023")>]
 [<assembly: AssemblyTitleAttribute("https://github.com/dotnet-websharper/plotly")>]
@@ -11,8 +11,8 @@ open System.Reflection
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyVersion = "8.0.0.0"
-    let [<Literal>] AssemblyFileVersion = "8.0.0.1"
+    let [<Literal>] AssemblyVersion = "9.0.0.0"
+    let [<Literal>] AssemblyFileVersion = "9.0.0.1"
     let [<Literal>] AssemblyCompany = "IntelliFactory"
     let [<Literal>] AssemblyCopyright = "(c) IntelliFactory 2023"
     let [<Literal>] AssemblyTitle = "https://github.com/dotnet-websharper/plotly"
